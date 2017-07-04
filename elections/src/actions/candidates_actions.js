53bd7f078f1726d17d5da970da108fc289a48894
tiramisu24 <diane.z.zheng@gmail.com>
2017-07-04 14:44:58 -0700

    update actions

--- elections/src/actions/candidates_actions.js
+++ elections/src/actions/candidates_actions.js
@@ -1,5 +1,7 @@
 import * as Util from '../util/api_calls';
 export const GET_ALL = "GET_ALL";
+export const ADD_CANDIDATE = "ADD_CANDIDATE";
+export const REMOVE_CANDIDATE = "REMOVE_CANDIDATE";
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
@@ -17,6 +19,15 @@ const getAllCandidateInfo = (candidates) => ({
   type: GET_ALL,
   candidates
 })
+const addCandidate = (candidate) => ({
+  type: ADD_CANDIDATE,
+  candidate
+})
+const removeCandidate = (candidate) => ({
+  type: REMOVE_CANDIDATE,
+  candidate
+})
+
 
 export const getAll = () => dispatch => {
   return Util.getAllCandidateInfo()
